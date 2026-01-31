9f32df8fe9df7de0b5c6e5c3252810c946806b9f
sadjad1366 <sadjad.ghasemi@gmail.com>
2026-01-31 12:14:48 +0330

    wip logic of counter

---
+++ app/counter-pro/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { ChangeEvent, SubmitEventHandler, useEffect, useState } from "react";
+
+export default function CounterPage() {
+  type ShowTime = {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+  const initialTime = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+  const [showTime, setShowTime] = useState<ShowTime>(initialTime);
+  const [remindedSeconds, setRemindedSeconds] = useState<number>(0);
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event?.target;
+    setShowTime((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const resetHandler = () => {
+    setIsActive(false);
+    setShowTime(initialTime)
+  }
+
+  const totalTime =
+    showTime.hours * 3600 + showTime.minutes * 60 + showTime.seconds;
+
+  useEffect(() => {
+    let timer;
+    if (isActive) {
+      timer = setInterval(() => {
+        setRemindedSeconds((prev) => {
+          if (prev <= 1) {
+            setIsActive(false);
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    }
+   if(timer) return () => clearInterval(timer)
+  }, [isActive, remindedSeconds]);
+
+  const submitHandler: SubmitEventHandler = (event) => {
+    event.preventDefault();
+    setRemindedSeconds(totalTime);
+    setIsActive(true);
+    console.log(remindedSeconds);
+  };
+  return (
+    <div className="space-x-4">
+      <form onSubmit={submitHandler}>
+        <input
+          className="border rounded-lg p-2 m-2 "
+          onChange={inputHandler}
+          type="text"
+          name="hours"
+          value={showTime.hours}
+          placeholder="00"
+        />
+        <input
+          className="border rounded-lg p-2 m-2 "
+          onChange={inputHandler}
+          type="text"
+          name="minutes"
+          value={showTime.minutes}
+          placeholder="00"
+        />
+        <input
+          className="border rounded-lg p-2 m-2 "
+          onChange={inputHandler}
+          type="text"
+          name="seconds"
+          value={showTime.seconds}
+          placeholder="00"
+        />
+        <button
+          type="submit"
+          className="bg-green-400 text-white rounded-lg p-2 m-2"
+        >
+          Start
+        </button>
+        <button onClick={resetHandler} className="bg-slate-400 text-white rounded-lg p-2 m-2">
+          Reset
+        </button>
+        <div>{remindedSeconds}</div>
+      </form>
+    </div>
+  );
+}
